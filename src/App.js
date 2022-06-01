9582de566e4c378f8faf3b24e1fefd497564c6fb
san0014s <san0014@mix.wvu.edu>
2022-06-01 19:19:05 -0400

    generated react app

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react"; //using react
+import "./index.css"; //import css idex
+import Home from "./routes/Home" //import pages of the website
+import About from "./routes/About"
+import Project from "./routes/Project"
+import Contact from "./routes/Contact"
+
+import { Route, Routes } from "react-router-dom";
+
+
+//We will be using Router which essentially is the standard libary of routes
+// for routing in react. This means it enables navigation among components and
+// and can allows the us (users) to change the browser url
+
+function App() {
+  return (
+    <>
+    <Routes> 
+      <Route path="/" element={<Home />} /> 
+      <Route path="/project" element={<About />} /> 
+      <Route path="/about" element={<Project />} /> 
+      <Route path="/contact" element={<Contact />} /> 
+    </Routes>
+    </>
+  );
+}
+
+export default App;
