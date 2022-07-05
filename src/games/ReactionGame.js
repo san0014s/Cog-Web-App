7dd5c44ca0e19c305320265028eabc68318aec65
Kryzstof Kudlak <Kryzstof.Kudlak@leidos.com>
2022-07-05 11:55:54 -0400

    TODOs, documentation, connected to storeData endpoint on backend server

--- src/games/ReactionGame.js
+++ src/games/ReactionGame.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styled from 'styled-components';
 import moment from 'moment';
 
+const GAME_TYPE = 1;
 const ROUNDS_TO_PLAY = 5;
 
 const NOT_YET = "Not Yet...";
@@ -73,9 +74,15 @@ export default function ReactionGame() {
         }
         else if (gameState === GameState.GameOver) {
             setButtonText(GAME_OVER);
+            recordData(GAME_TYPE, getAverage());
         }
     }, [gameState]); // putting the gameState variable here means that the useEffect will watch this var and react to any updates that happen to it
 
+    /**
+     * Changes button state when clicked. If the game is "in progress" (i.e. we're timing the player), then we record
+     *  the time that they clicked and transition to the "Game Completed" state. If the game hasn't yet started and the
+     *  player clicks early, we switch to the "Too Early" state
+     */
     function onClickFunction() {
         console.log("button was clicked");
         if (gameState === GameState.InProgress) {
@@ -87,11 +94,37 @@ export default function ReactionGame() {
         }
     }
 
+    /**
+     * Takes the average of the timeDiffList and returns it. If there's nothing in the list, it returns
+     *  an "X" character because that's what I want displayed on the screen
+     * 
+     * @returns - the average of timeDiffList or the character 'X' if that list is empty
+     */
     function getAverage() {
         if (timeDiffList.length === 0)
             return "X"
         else
-            return timeDiffList.reduce((a, b) => a + b) / timeDiffList.length;
+            return timeDiffList.reduce((a, b) => a + b) / timeDiffList.length; // lol stack overflow
+    }
+
+    /**
+     * Hits the storeData endpoint of the REST server to store a stat and gameType
+     * 
+     * @param {int} gameType - enumerable property representing the game type (i've arbitrarily chosen 1 for this game, TODO: standardize that server side)
+     * @param {num} stat - whatever stat needs recorded for this game
+     */
+    function recordData(gameType, stat) { // TODO this should be a utility function used by all games & thus shouldn't be contained in this component in the future
+        let personalData = {
+            "gameType":gameType,
+            "stat":stat,
+            "accountId":24 //TODO: replace with session prop
+        }
+
+        fetch('http://localhost:8080/storeData', { // TODO: make protocol, ip address, and port(?) configurable
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(personalData)
+        }); // TODO: possibly apply a .then() and .catch() or return the promise so that callers can handle .then and/or .catch
     }
 
     return <>
