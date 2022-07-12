1187b5a2d065ebb41a435b14847ecbad93be5f7c
karlscheib <kes0049@mix.wvu.edu>
2022-07-12 16:55:39 -0400

    improved home screen look, still needs work

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import {Routes, Route, useNavigate} from 'react-router-dom'
+import styled from 'styled-components';
 
 //the following constants are what prints out for each button and the home page greeting
 const greeting = "Welcome to the Cognitive Web Application";
@@ -8,6 +9,46 @@ const AccountButton = "Go to Your Account";
 const GamesButton = "Go to Games";
 const ContactButton = "Contact Us!";
 const UpdateButton = "Updates";
+
+//different button themes (just one right now)
+const theme = { 
+  black:{
+    default: '#212121',
+    hover: '#424242',
+    textColor: '#fafafa'
+  }
+}
+
+const Button = styled.button`
+  background-color: ${props => theme[props.theme].default};
+  color: ${props => theme[props.theme].textColor};
+  font-family: Arial, sans-serif;
+  font-size: 1.5em;
+  outline: 0;
+  cursor: pointer;
+  margin: 10px 10px;
+  width: 18vw;
+  height: 10vh;
+  transition: ease background-color 250ms;
+
+  //what happens when the user hovers over the button
+  &:hover {
+    background-color: ${props => theme[props.theme].hover};
+  }
+
+  //tag used to disable a button
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+    &:hover {
+      background-color: ${props => theme[props.theme].default};
+    }
+  }
+`
+
+Button.defaultProps = {
+  theme: 'black'
+}
 
 export default function Home() {
 
@@ -37,24 +78,24 @@ export default function Home() {
 
   return <>
     <div>{greeting}</div>
-    <button onClick = {navigateAboutUs}>
+    <Button onClick = {navigateAboutUs}>
       {AboutButton}
-    </button>
+    </Button>
 
-    <button onClick = {naviagateAccount}>
+    <Button onClick = {naviagateAccount}>
       {AccountButton}
-    </button>
+    </Button>
 
-    <button onClick = {naviagateGames}>
+    <Button onClick = {naviagateGames}>
       {GamesButton}
-    </button>
+    </Button>
 
-    <button onClick = {naviagateContactUs}>
+    <Button onClick = {naviagateContactUs}>
       {ContactButton}
-    </button>
+    </Button>
 
-    <button onClick = {navigateUpdates}>
+    <Button onClick = {navigateUpdates}>
       {UpdateButton}
-    </button>
+    </Button>
   </>
 }
