503126111c398b61e875a07b635a98f3ec9d7a00
karlscheib <kes0049@mix.wvu.edu>
2022-07-27 15:35:35 -0400

    created Login and Signup routes and fixed routes tutorial

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Updates from "./routes/Updates"
 import Contact from "./routes/Contact"
 import Account from "./routes/Account"
 import Project from "./routes/Project"
+import Login from "./routes/Login"
+import Signup from "./routes/Signup"
 import ReactionGame from "./games/ReactionGame"
 import SessionState from "./components/SessionState"
 
@@ -21,17 +23,17 @@ function App() {
   SessionState.setId(24); // TODO this should be set on login
 
   return (
+    // The Routes tag includes all the pages that can be accessed in the app. To add a new page, do the following
+    // 1. Create a new .js file somewhere (conventionally in the games or routes folder) and write code for that page (see any other page for an example)
+    // 2. At the top of this file, write...
+    //        import name-of-page from "relative-path-to-page"
+    //
+    // 3. In the routes tag, write... 
+    //        <Route path="name-in-url" element={<name-of-page/>} />
+    // Now while the app is running, in the URL, add the "name-in-url" and your new page should appear
+    // Hope this helps :)
+    //
     <>
-    /* The Routes tag includes all the pages that can be accessed in the app. To add a new page, do the following
-    * 1. Create a new .js file somewhere (conventionally in the games or routes folder) and write code for that page (see any other page for an example)
-    * 2. At the top of this file, write...
-    *        import [name-of-page] from "[relative-path-to-page]"
-    *
-    * 3. In the routes tag, write... 
-    *        <Route path="[name-in-url]" element={<[name-of-page]/>} />
-    * Now while the app is running, in the URL, add the "[name-in-url]" and your new page should appear
-    * Hope this helps :)
-    */
     <Routes> 
       <Route path="/" element={<Home />} /> 
       <Route path="/updates" element={<Updates />} /> 
@@ -40,6 +42,8 @@ function App() {
       <Route path="/account" element={<Account />} /> 
       <Route path="/project" element={<Project />} /> 
       <Route path="/reactionGame" element={<ReactionGame />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
     </>
   );
