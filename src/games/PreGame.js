d37e3a3fa4b657c7d52e579d78b0752d32871a70
Kryzstof Kudlak <Kryzstof.Kudlak@leidos.com>
2022-08-29 12:37:44 -0400

    get rid of remaining red text in the console

--- src/games/PreGame.js
+++ src/games/PreGame.js
@@ -4,12 +4,10 @@ import { Button } from "@mui/material";
 export default function PreGame({ game, advanceStateFunction }) {
 
     return <>
-        <body>
         <h1>{game.display}</h1>
         <h4>{game.description}</h4>
         <p>How to play: {game.howTo}</p>
         <Button onClick={advanceStateFunction}>Play</Button>
-        </body>
     </>
 
 }
