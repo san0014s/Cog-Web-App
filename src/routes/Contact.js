73e6fe4766299f5fd2106466a211dbcdcd24f9dd
Kryzstof Kudlak <Kryzstof.Kudlak@leidos.com>
2022-09-21 09:55:45 -0400

    fix visual bugs, make a classname for contact button

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -5,7 +5,7 @@ import '../css/Contact.css';
 //TODO: need icons for phone numbers, emails, and linkedin (look into materialUI)
 export default function Contact() {
   return (
-    <body className="Contact-body">
+    <div className="Contact-body">
       <h1 className="Contact-h1">Contact Us!</h1>
       <p className="sub-title">If you have any inquiries, please use the contact form below.</p>
 
@@ -14,22 +14,20 @@ export default function Contact() {
 
         <div className="Contact-info">
           <h3>Contact Information</h3>
-          <p className="Contact-p2">text</p>
           <div className="icon-Text">
-            <i class="fa fa-phone" aria-hidden="true"></i>
+            <i className="fa fa-phone" aria-hidden="true"></i>
             <span>Steven Navarro: 304-702-9445 </span>
           </div>
           <div className="icon-Text">
-            <i class="fa fa-envelope" aria-hidden="true"></i>
+            <i className="fa fa-envelope" aria-hidden="true"></i>
             <span className="Contact-span">Steven Navarro: san0014@mix.wvu.edu</span>
           </div>
-        </div>
-
-
-        <div className="social-media">
-          <a href="#" className="icon-circle">
-            <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-          </a>
+          
+          <div className="social-media">
+            <a href="#" className="icon-circle">
+              <i className="fa fa-linkedin-square" aria-hidden="true"></i>
+            </a>
+          </div>
         </div>
 
 
@@ -40,54 +38,54 @@ export default function Contact() {
               <input className="Contact-textinput" type="text"></input>
             </div>
             <div className="form-group">
-              <label>Last Name</label>
+              <label className="Contact-label">Last Name</label>
               <input className="Contact-textinput" type="text"></input>
             </div>
           </div>
           <div className="column">
             <div className="form-group">
-              <label>Email</label>
+              <label className="Contact-label">Email</label>
               <input className="Contact-emailinput" type="email"></input>
             </div>
             <div className="form-group">
-              <label>Phone Number</label>
+              <label className="Contact-label">Phone Number</label>
               <input className="Contact-telinput" type="tel"></input>
             </div>
           </div>
           <div className="column">
             <div className="form-group solo">
-              <label>Please choose the category that best fits your inquiry.</label>
+              <label className="Contact-label">Please choose the category that best fits your inquiry.</label>
               <div id="radio-buttons">
                 <div className="radio-button">
-                  <input className="Contact-rb" type="radio" id="radiobugs" name="type" value="Bugs"></input><label for="radiobugs">Bugs</label>
+                  <input className="Contact-rb" type="radio" id="radiobugs" name="type" value="Bugs"></input><label htmlFor="radiobugs">Bugs</label>
                 </div>
                 <div className="radio-button">
-                  <input className="Contact-rb" type="radio" id="radioproject" name="type" value="Project Questions"></input><label for="radioproject">Project Questions</label>
+                  <input className="Contact-rb" type="radio" id="radioproject" name="type" value="Project Questions"></input><label htmlFor="radioproject">Project Questions</label>
                 </div>
                 <div className="radio-button">
-                  <input className="Contact-rb" type="radio" id="radiogames" name="type" value="Games"></input><label for="radiogames">Games</label>
+                  <input className="Contact-rb" type="radio" id="radiogames" name="type" value="Games"></input><label htmlFor="radiogames">Games</label>
                 </div>
                 <div className="radio-button">
-                  <input className="Contact-rb" type="radio" id="radiofamily" name="type" value="Family Flash Cards"></input><label for="radiofamily">Family Flash Cards</label>
+                  <input className="Contact-rb" type="radio" id="radiofamily" name="type" value="Family Flash Cards"></input><label htmlFor="radiofamily">Family Flash Cards</label>
                 </div>
               </div>
             </div>
           </div>
           <div className="column">
             <div className="form-group solo">
-              <label>Message</label>
+              <label className="Contact-label">Message</label>
               <textarea></textarea>
             </div>
           </div>
           <div className="column">
             <div className="form-group solo right">
-              <button className="primary">Send Message</button>
+              <button className="primary Contact-button">Send Message</button>
             </div>
           </div>
         </form>
 
       </div>
-  </body>
+  </div>
   )
 }
 
