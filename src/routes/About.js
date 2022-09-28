a34a048f98f55fbe78e5ef22582e8ff9c909385f
nsawv <nsa0007@mix.wvu.edu>
2022-09-28 15:23:04 -0400

    updates to about page & styling, stevens blurb

--- src/routes/About.js
+++ src/routes/About.js
@@ -9,68 +9,66 @@ const About = () => {
 
       <div class="wrapper">
         <h1> Meet Our Team </h1>
-        
-        <div class="group">
-          <div class="group_member">
+        <div class="general"></div>
+        <h1> Test Test Test Test </h1>
+          <div class="group">
+            <div class="group_member">
+                <div class="team_img">
+                    <img src="https://cog-web-app-public-assets.s3.amazonaws.com/Headshots/NSA.JPG" alt="nsa_image"></img>
+                </div>
+                  <h3>Nate Arndt</h3>
+                  <p class="role">Front End</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni!</p>
+            </div>
+
+            <div class="group_member">
               <div class="team_img">
-                  <img src="https://cog-web-app-public-assets.s3.amazonaws.com/Headshots/NSA.JPG" alt="nsa_image"></img>
+                    <img src="https://cog-web-app-public-assets.s3.amazonaws.com/Headshots/SAN.jpg" alt="nsa_image"></img>
               </div>
-                <h3>Nate Arndt</h3>
-                <p class="role">Front End</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni!</p>
-          </div>
-
-          <div class="group_member">
-            <div class="team_img">
-                  <img src="https://cog-web-app-public-assets.s3.amazonaws.com/Headshots/SAN.jpg" alt="nsa_image"></img>
+                  <h3>Steven Navarro</h3>
+                  <p class="role">Front End</p>
+                  <p>I am from Martinsburg, WV and attend the Statler College in WVU. 
+                    I am a cybersecurity major with minors in criminology and computer science.
+                     My future goal is to work as a penetration tester.</p>
             </div>
-                <h3>Steven Navarro</h3>
-                <p class="role">Front End</p>
-                <p>I am Steven Navarro from Martinsburg, West Virginia. Currently enrolled
-                   in West Virginia University pursuing a Cybersecurity degree with minors
-                    in Criminology and Computer Science. Passion involved web development and
-                     cybersecurity/IT. I enjoy challenges in life that give me purpose and
-                      makes me want to do more. Being active and constantly learning is 
-                      something I always make myself due</p>
-          </div>
 
-          <div class="group_member">
-            <div class="team_img">
-                <img src="https://cog-web-app-public-assets.s3.amazonaws.com/Headshots/KJK.png" alt="kk_image"></img>
+            <div class="group_member">
+              <div class="team_img">
+                  <img src="https://cog-web-app-public-assets.s3.amazonaws.com/Headshots/KJK.png" alt="kk_image"></img>
+              </div>
+                  <h3>Krysztof Kudlak</h3>
+                  <p class="role">Front End</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni!</p>
             </div>
-                <h3>Krysztof Kudlak</h3>
-                <p class="role">Front End</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni!</p>
-          </div>
 
-          <div class="group_member">
-            <div class="team_img">
-                <img src="https://cog-web-app-public-assets.s3.amazonaws.com/Headshots/KES.png" alt="ks_image"></img>
+            <div class="group_member">
+              <div class="team_img">
+                  <img src="https://cog-web-app-public-assets.s3.amazonaws.com/Headshots/KES.png" alt="ks_image"></img>
+              </div>
+                  <h3>Karl Scheib</h3>
+                  <p class="role">Front End</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni!</p>
             </div>
-                <h3>Karl Scheib</h3>
-                <p class="role">Front End</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni!</p>
-          </div>
 
-          <div class="group_member">
-            <div class="team_img">
-                <img src="https://cog-web-app-public-assets.s3.amazonaws.com/Headshots/jt.jpg" alt="jt_image"></img>
+            <div class="group_member">
+              <div class="team_img">
+                  <img src="https://cog-web-app-public-assets.s3.amazonaws.com/Headshots/jt.jpg" alt="jt_image"></img>
+              </div>
+                  <h3>Jake Taylor</h3>
+                  <p class="role">Front End</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni!</p>
             </div>
-                <h3>Jake Taylor</h3>
-                <p class="role">Front End</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni!</p>
+
+
           </div>
-
-
-        </div>
 
       </div>
 
