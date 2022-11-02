2a9dda8306e8591d69943f771682261075d2d291
karlscheib <kes0049@mix.wvu.edu>
2022-11-02 17:01:50 -0400

    started better implementation of edit page (needs work)

--- src/routes/FFC_Edit.js
+++ src/routes/FFC_Edit.js
@@ -1,47 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import {Routes, Route, useNavigate} from 'react-router-dom';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 
 export default function FFC_Edit() {
-  const [deckName, setDeckName] = useState('');
-  const [cardName, setCardName] = useState('');//this is not the correct way to do this
-  const [selectedImage, setSelectedImage] = useState('');
-  const [uploadData, setUploadData] = useState('');
+
+  
+
+  const defaultCardValues = {
+    name: "",
+    images: [],//what should this be?
+  }
+  const [deckName, setDeckName] = useState();
+  const [cards, setCards] = useState([defaultCardValues]);
+  const navigate = useNavigate();
 
   function handleOnSubmit(e) {
     e.preventDefault();
     console.log("Submitted");
+    console.log(deckName);
+    console.log(cards);
     //TODO set up to backend
   }
 
-  return<>
-    <form onSubmit={(e) => { handleOnSubmit(e) }}>
-      <label >
-        Deck Name
-      </label><br />
-      <input type="text" value={deckName} required onChange={(e)=> setDeckName(e.target.value)} /><br />
-      <div>
-        {selectedImage && (
-          <div>
-          <img alt="not found" width={"250px"} src={URL.createObjectURL(selectedImage)} /> <br />
-          <input type="text" required onChange={(e)=> setCardName(e.target.value)} /> <br />
-          <button onClick={()=>setSelectedImage(null)}>Remove</button>
+  const handleCardChange = (e) => {
+    console.log(e);
+    if(e.target.files) {
+      setCards({ ...cards, images: [...e.target.files]})
+    }
+  }
 
-          </div>
-        )} <br />
-        
-       
-        <input
-          type="file"
-          name="myImage"
-          multiple accept="image/*"
-          onChange={(event) => {
-            console.log(event.target.files[0]);
-            setSelectedImage(event.target.files[0]);
-          }}
-        />
-        
-      </div>
-      <input type="submit" value="Submit" />
-    </form>
-  
+  const handleDeckName = (e) => {
+    setDeckName(e.target.value)
+  }  
+  function onRemoveImage(e: any) {
+    //TODO: remove card from deck
+    console.log(e);
+  }
+  return<>
+    <TextField
+      id="deck-input"
+      name="deck-name"
+      label="Deck Name"
+      type="text"
+      value={deckName}
+      onChange={handleDeckName}
+    />
+    <br />
+
+    <ImageList sx={{ width: 500, height: 450 }}> 
+      {cards.map((card) => (
+        <ImageListItem key={card.image}>
+          <img 
+            src={`${card.image}?w=248&fit=crop&auto=format`}
+            srcSet={`${card.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+            alt={card.name}
+            loading="lazy"
+          />
+          <ImageListItemBar
+            title={card.name}
+            position="below"
+          />
+        </ImageListItem>
+
+      ))}
+    </ImageList>
+    <br /> 
+    <button onClick={(e)=> handleCardChange(e)}> Upload Images</button>
+    <br />
+    <button onClick={(e)=> handleOnSubmit(e)}>Submit Changes</button>
+    <button onClick={() => navigate('/account')}>Cancel</button>
   </>
 }
