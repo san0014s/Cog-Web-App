8a5cf6eb28d0ab55bef8ed4739a95470a26617bf
Kryzstof Kudlak <Kryzstof.Kudlak@leidos.com>
2022-11-16 15:39:12 -0500

    sliding puzzle game pictures

--- src/games/SlidingPuzzle.js
+++ src/games/SlidingPuzzle.js
@@ -172,12 +172,19 @@ export default function SlidingPuzzle({ advanceStateFunction }) {
                     backgroundColor: "skyblue",
                     borderRadius: 25,
                     cursor: complete ? "not-allowed" : "pointer",
-                    userSelect: "none"
+                    userSelect: "none",
                   }}
                 >
-                  <span style={{ fontSize: "2rem", fontWeight: "bold" }}>
+                  {(col !== 0 &&
+                    <img
+                      src={`${process.env.PUBLIC_URL}/sliding_puzzle_images/image_part_00${col}.png`}
+                      width="100%"
+                      height="100%"
+                    />
+                  )}
+                  {/* <span style={{ fontSize: "2rem", fontWeight: "bold" }}>
                     {col !== 0 && col}
-                  </span>
+                  </span> */}
                 </div>
               );
             })}
