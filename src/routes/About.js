205e7e4a8f3d034fc14dee745036e602461ddcff
nsawv <nsa0007@mix.wvu.edu>
2022-11-16 14:59:49 -0500

    adding bios to about us

--- src/routes/About.js
+++ src/routes/About.js
@@ -7,10 +7,14 @@ const About = () => {
   return (
     <body>
 
+<head>
+    <title>About us Page</title>
+</head>
+
       <div class="wrapper">
         <h1> Meet Our Team </h1>
         <div class="general"></div>
-        <h1> Test Test Test Test </h1>
+        {/* <h1> Test Test Test Test </h1> */}
           <div class="group">
             <div class="group_member">
                 <div class="team_img">
@@ -30,9 +34,12 @@ const About = () => {
               </div>
                   <h3>Steven Navarro</h3>
                   <p class="role">Front End</p>
-                  <p>I am from Martinsburg, WV and attend the Statler College in WVU. 
-                    I am a cybersecurity major with minors in criminology and computer science.
-                     My future goal is to work as a penetration tester.</p>
+                  <p>I am Steven Navarro from Martinsburg, West Virginia. Currently enrolled
+                   in West Virginia University pursuing a Cybersecurity degree with minors
+                    in Criminology and Computer Science. Passion involved web development and
+                     cybersecurity/IT. I enjoy challenges in life that give me purpose and
+                      makes me want to do more. Being active and constantly learning is 
+                      something I always make myself do</p>
             </div>
 
             <div class="group_member">
@@ -52,9 +59,9 @@ const About = () => {
               </div>
                   <h3>Karl Scheib</h3>
                   <p class="role">Front End</p>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-                    sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris ni!</p>
+                  <p>I am currently Computer Engineering undergraduate at West Virginia University with a minor in Mathematics.
+                Aside from my degree, I also enjoy game development and design. I have a deep appreciation for projects and fields
+                that involve many people with a variety of talents and experiences.</p>
             </div>
 
             <div class="group_member">
